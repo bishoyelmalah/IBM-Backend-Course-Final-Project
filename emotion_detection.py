c5a7f39fffc8760e57736befe979fca3b013cf99
bishoyelmalah <bishoy.elmalah@gmail.com>
2026-02-25 12:18:31 -0500

    edit emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,15 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse):
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     my_Obj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = my_Obj, headers = headers)
-    return response.text
+    formatted_res = json.loads(response.text)
+
+    emotions_dict = formatted_res["emotionPredictions"][0]["emotion"]
+    dominant_emotion = max(emotions_dict, key=emotions_dict.get)
+    emotions_dict["dominant_emotion"] = dominant_emotion
+
+    return emotions_dict
